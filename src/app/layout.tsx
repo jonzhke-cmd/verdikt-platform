58d127f10d4dc549e60a64d301df4e3a9ee59af8
Jon <jonzhke@gmail.com>
2026-04-05 15:03:50 +0000

    Fix missing Link import in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 import { AuthProvider } from '@/contexts/AuthContext'
 import NavClient from '@/components/NavClient'
 
